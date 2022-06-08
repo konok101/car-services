c33abdf38b2f0116f9f66a981a99f9d441617bd8
konok101 <rockykonok@gmail.com>
2022-06-09 02:11:10 +0600

    responsive part done with css

--- src/component/Pages/Home/Service/Service.js
+++ src/component/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
-const Service = ({service}) => {
-    const {name, img, description, price}=service;
+const Service = ({ service }) => {
+    const { name, img, description, price } = service;
     return (
-        <div>
+        <div className='service-part'>
             <img src={img} alt="" />
-           {name}
+            <h2> {name}</h2>
+            <p>Price: {price}</p>
+            <p><small>{description}</small></p>
+            <button>Book: {name}</button>
         </div>
     );
 };
