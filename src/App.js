e19265a5f62f923e51cfe25e4b8bc7dcafdb9e63
konok101 <rockykonok@gmail.com>
2022-06-09 04:43:30 +0600

    route dynamic part done with use navigate for link and params for link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './component/Pages/About/About';
 import Home from './component/Pages/Home/Home/Home';
 import Header from './component/Pages/Shared/Header/Header';
 import Footer from './component/Pages/Shared/Footer/Footer';
+import ServiceDetails from './component/Pages/ServiceDetails/ServiceDetails';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
