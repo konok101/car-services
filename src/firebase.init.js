394c9ba06211791821559ea2b124f489f0665e5f
konok101 <rockykonok@gmail.com>
2022-06-10 00:04:34 +0600

    github ignore  gitInitfirebase part

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrjF2JhaEWup6fZ5iaf5hzc3FYM-gvNkU",
-  authDomain: "genius-car-services-50d3f.firebaseapp.com",
-  projectId: "genius-car-services-50d3f",
-  storageBucket: "genius-car-services-50d3f.appspot.com",
-  messagingSenderId: "387242210960",
-  appId: "1:387242210960:web:0b13bb757e8e5539b60a15"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_appId,
+  appId:process.env.REACT_APP_messagingSenderId
 };
 
 // Initialize Firebase
