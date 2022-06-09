46dc282e7a0da07307833095408145e4f15dc0b9
konok101 <rockykonok@gmail.com>
2022-06-10 01:12:05 +0600

    login and signup auth part done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './component/Pages/Shared/Footer/Footer';
 import Login from './component/Pages/Login/Login'
 import ServiceDetails from './component/Pages/ServiceDetails/ServiceDetails';
 import Register from './component/Pages/Login/Register/Register';
+import NotFond from './component/NotFond/NotFond';
 
 function App() {
   return (
@@ -16,10 +17,12 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFond></NotFond>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/component/Pages/Login/Login.js
+++ src/component/Pages/Login/Login.js
@@ -1,16 +1,32 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import {   useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 
 const Login = () => {
   const emailRef = useRef('');
   const passRef = useRef('');
   const navigate = useNavigate();
-  const handleSubmit = event => {
-    const email = emailRef.current.value;
-    const password = passRef.current.value;
+ 
+    const [
+      signInWithEmailAndPassword,
+      user,
+      loading,
+      error,
+    ] = useSignInWithEmailAndPassword(auth);
+if(user){
+  navigate('/home');
+}
 
-  }
+    const handleSubmit = event => {
+      event.preventDefault();
+      const email = emailRef.current.value;
+      const password = passRef.current.value;
+      signInWithEmailAndPassword(email, password);
+    }
+
+  
 
   const navigateRegister = event => {
     navigate('/register');
