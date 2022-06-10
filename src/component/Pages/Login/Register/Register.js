5d3462a7ff3ba8cc91fd071a876779e5c160f430
konok101 <rockykonok@gmail.com>
2022-06-10 16:11:38 +0600

    if checkbox not click then register button disable

--- src/component/Pages/Login/Register/Register.js
+++ src/component/Pages/Login/Register/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import './Register.css'
 import auth from '../../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
@@ -12,7 +12,9 @@ const Register = () => {
         user,
         loading,
         error,
-    ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
     const navigate = useNavigate();
 
     const navigateLoigin = () => {
@@ -41,8 +43,8 @@ const Register = () => {
                 <input type="email" name="email" id="" placeholder='your email ' required />
                 <input type="password" name="password" placeholder='password' required id="" />
                 <input  onClick={()=>setAgree(!agree)} type="checkbox" name="terms" id="terms" />
-                <label className={agree ? 'text-primary' : 'text-danger'} htmlFor="terms">Aecpts Genius conditions</label>
-                <input type="submit" value="Register" />
+                <label className={`ps-2 ${agree ? 'text-primary' :'text-danger' } `} text-danger htmlFor="terms">Aecpts Genius conditions</label>
+                <input disabled={!agree} type="submit" value="Register" />
 
             </form>
             <p>Already have an account? <Link to="/Login" className='text-danger pe-auto text-decoration-none' onClick={navigateLoigin}>Please Login</Link></p>
